eaeb9b06d0d23f0c39408a387b18d007fe9d6749
leidyz <leidy.zhang98@gmail.com>
2023-11-16 14:49:26 +0100

    migration of games and foreign keys

---
+++ Sports_League/app/Models/Game.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    //One to Many relationship(inverse)
+    public function localTeam(){
+        return $this->belongsTo(Team::class,'local_team');
+    }
+
+    public function guestTeam(){
+        return $this->belongsTo(Team::class,'guest_team');
+    }
+}
