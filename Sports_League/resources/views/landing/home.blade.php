a29b4d8dfc7271ecb9dc2ff98dc28355a59229e7
leidyz <leidy.zhang98@gmail.com>
2023-11-28 09:53:55 +0100

    home pg: Change games background

--- Sports_League/resources/views/landing/home.blade.php
+++ Sports_League/resources/views/landing/home.blade.php
@@ -19,13 +19,13 @@
     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
          <!-- Item 1 -->
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="{{asset('/images/kobe_cta.png')}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 flex-row" alt="...">
-            <div><h1 class="absolute inset-y-0 right-0 mb-4 text-3xl font-extrabold text-white-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">NBA Basketball League</span></h1></div>
-            <div class="absolute top-1/2 transform translate-y-1/2 bottom-0 right-0 mb-4 me-4">
+            <img src="{{asset('/images/matches.png')}}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 flex-row" alt="...">
+            <div><h1 class="absolute inset-y-0 right-0 mb-4 text-3xl font-extrabold text-white-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"></span></h1></div>
+            <div class="absolute p-4 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
                 <!-- Button is now below "NBA Basketball Team" and centered vertically -->
                 <button class="relative inline-flex items-center justify-center p-2.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
                     <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    <a href="{{route('teams.create')}}">Create a New Game</a>
+                    <a href="{{route('games.create')}}">Create a New Game</a>
                     </span>
                 </button>
             </div>
